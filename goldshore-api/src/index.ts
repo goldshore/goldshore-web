ba95f000ee61f008bd032b02fccf4e824ced9eb2
Bobby Rosenberg <marstonr6@gmail.com>
2025-10-15 22:02:58 -0400

    Default json helper headers to object

--- goldshore-api/src/index.ts
+++ goldshore-api/src/index.ts
@@ -23,7 +23,7 @@ const pickOrigin = (env: Env, req: Request) => {
 }
 
 const json = (data: unknown, init: ResponseInit = {}) => {
-  const headers = new Headers(init.headers)
+  const headers = new Headers(init.headers ?? {})
   headers.set("Content-Type", "application/json; charset=utf-8")
 
   return new Response(JSON.stringify(data), {
